0e9c68d5a2bd472c73e3f78827d87b2ffe739838
Pawel Kijowski <pkijowsk@us.ibm.com>
2017-04-18 11:55:45 -0500

    Initial Commit

---
+++ UIImagePDFSupport/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIImagePDFSupport
+//
+//  Created by Pawel Kijowski Silver on 4/18/17.
+//  Copyright © 2017 Pawel Kijowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
